0b22c4510445cd2088c85d1894f8a3c5e150d55e
Nikita Aristov <nikitaaristov84@mail.ru>
2025-06-03 15:47:17 +0500

    adaptive layout

--- book_tour.php
+++ book_tour.php
@@ -253,6 +253,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Бронирование тура | <?= htmlspecialchars($tour['tour_name']) ?></title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
